8909679dbf6851e32bbdebfd6159136460e14836
Steve Fielding <sfielding@base2designs.com>
2018-03-26 09:47:03 -0700

    Rewrote plate detector and char classifier. Now classifys chars in every plate that is detected. Plate text is added to a new history buffer, and the best plates are selected from this histtory buffer

--- base2designs/utils/charClassifier.py
+++ base2designs/utils/charClassifier.py
@@ -4,18 +4,12 @@ import cv2
 import re
 
 class CharClassifier:
-  def __init__(self, labelProcessor, charClassifierModel, output_image_path,
-               output_cropped_image_path, logFile, saveAnnotatedImage=False, preprocessors=None, croppedImagePreprocessors=None):
+  def __init__(self, labelProcessor, charClassifierModel, preprocessors=None, croppedImagePreprocessors=None):
     # store the labelProcessor
     self.labelProcessor = labelProcessor
 
     # store the charClassifierModel
     self.charClassifierModel = charClassifierModel
-
-    self.output_image_path = output_image_path
-    self.output_cropped_image_path = output_cropped_image_path
-    self.logFile = logFile
-    self.saveAnnotatedImage = saveAnnotatedImage
 
     # store the image preprocessors
     self.preprocessors = preprocessors
@@ -40,35 +34,15 @@ class CharClassifier:
     return image
 
 
-  # Find all the bounding boxes for the plates in the input image
-  # For every bounding box
-  #   Crop the plate from the image
-  #   Apply image processing to cropped plate
-  #   Apply charClassifierModel to predict plate text
-  #   Append the plate text to plateList
-  #   Optionally annotate the original image with bounding box and plate text
-  #   print the plate text
-  #   save the image
-  # Return plateList
-  def findCharsInPlate(self, bestImage, licensePlateList, outputFileName, destFolderRootName, frameCount, videoFileName, margin=0, imageDebugEnable=False):
-    outputFullImageFileName = outputFileName
+  # pre-process all the plateImages, and predict the plate text
+  def predictPlateText(self, plateImages):
     plateList = []
-    lpBoxes = [i[2] for i in licensePlateList]
-    for lpBoxPts in lpBoxes:
-
-      # add some extra margin. Useful if we want to apply image augmentation during training
-      t = lpBoxPts[0][1]
-      b = lpBoxPts[1][1]
-      l = lpBoxPts[0][0]
-      r = lpBoxPts[1][0]
-      (top, left) = (np.array([t, l]) - margin).clip(0)
-      bottom = (b + margin).clip(0, bestImage.shape[0])
-      right = (r + margin).clip(0, bestImage.shape[1])
-      plateImage = bestImage[top:bottom, left:right]
+    plateImagesPreProcessed = []
+    for plateImage in plateImages:
+      # pre-process the plate image
       plateImageProcessed = self.processImage(plateImage, self.preprocessors)
 
       # get the predictions, and create clean one-hot predictions
-      print("[INFO] display some results...")
       preds = self.charClassifierModel.predict(np.array([plateImageProcessed]), batch_size=32)
       argMax = preds.argmax(axis=-1)
       predsClean = np.zeros_like(preds, dtype=np.int)
@@ -82,37 +56,9 @@ class CharClassifier:
       predPlateText = self.labelProcessor.inverse_transform(predsClean)
       predPlateText = ''.join(predPlateText[0])
       plateList.append((predPlateText))
-      outputFullImageFileName += '_' + predPlateText
+      # print("  plate: {}".format(predPlateText))
 
-      # write cropped plate image to file, and append plate text to the file name
-      # Mimic the file name format used by Supervisely (almost)
-      outputCroppedPath = "{}/{}/{}.png".format(self.output_cropped_image_path, destFolderRootName, outputFileName + '_' + predPlateText + '_1')
-      cv2.imwrite(outputCroppedPath, self.processImage(plateImage, self.croppedImagePreprocessors))
+      # convert image to grey scale and resize to 128x64
+      plateImagesPreProcessed.append(self.processImage(plateImage, self.croppedImagePreprocessors))
 
-      # optionally annotate the image
-      if self.saveAnnotatedImage == True:
-        cv2.rectangle(bestImage, lpBoxPts[0], lpBoxPts[1], (0, 255, 0), 2)
-        cv2.putText(bestImage, predPlateText, lpBoxPts[0], cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
-      # print the plate text
-      print("  plate: {}".format(predPlateText))
-
-    # save the image
-    print("[INFO] logging image to file: {}".format(outputFullImageFileName))
-    outputPath = "{}/{}/{}.jpg".format(self.output_image_path, destFolderRootName, outputFullImageFileName)
-    cv2.imwrite(outputPath, bestImage)
-
-    # update the log file
-    # videoFileName, imageFileName, date, time, frameNumber, plateText
-    # lplate_toy_video4.mp4,2018_01_01/lplate_toy_video.mp4_51.jpg,2018_01_10,5:05,271,5HUY634,5JHY768
-    imageFileName = "{}/{}.jpg".format(destFolderRootName, outputFullImageFileName)
-    date = destFolderRootName
-    m = re.search(r"^([0-9]{2}[.:][0-9]{2}[.:][0-9]{2})",videoFileName)
-    if m:
-      time = m.group(1)
-    else:
-      time = "HH.MM.SS"
-    self.logFile.write("{},{},{},{},{},{}\n".format(videoFileName, imageFileName, date, time, frameCount, ','.join(plateList)))
-    self.logFile.flush()
-
-    return plateList
-
+    return (plateList, plateImagesPreProcessed)
---
+++ base2designs/utils/plateHistory.py
@@ -0,0 +1,177 @@
+
+import copy
+import numpy as np
+import cv2
+import re
+
+class PlateHistory:
+
+  def __init__(self, output_image_path, output_cropped_image_path, logFile, saveAnnotatedImage, rollingDictHistoryLimit):
+    self.rollingPlateDict = {}
+    self.fileCnt = 1
+    self.output_image_path = output_image_path
+    self.output_cropped_image_path = output_cropped_image_path
+    self.saveAnnotatedImage = saveAnnotatedImage
+    self.logFile = logFile
+    self.rollingDictHistoryLimit = rollingDictHistoryLimit
+
+  # clear the rollingPlateDict. Need to do this at the start of every new video clip
+  def clearHistory(self):
+    self.rollingPlateDict = {}
+
+  # plateList: list of plate texts
+  # plateImages: list of cropped plate images
+  # fullImage: Original image from which the plates were cropped
+  # videoPath: path to the image
+  # frameNumber: video clip frame number
+  # rollingPlateDict has the following format:
+  # key = plateText
+  #   val = [ [plateText, plateImage, plateBox, fullImage, videoPath, frameNumber],
+  #           [plateText, plateImage, plateBox, fullImage, videoPath, frameNumber] ...]
+  def addPlatesToHistory(self, plateList, plateImages, plateBoxes, fullImage, videoPath, frameNumber):
+    # add the new plates to rollingPlateDict
+    for (plateText, plateImage, plateBox) in zip(plateList, plateImages, plateBoxes):
+      if plateText in self.rollingPlateDict:
+        self.rollingPlateDict[plateText].append([plateText, plateImage, plateBox, fullImage, videoPath, frameNumber])
+      else:
+        self.rollingPlateDict[plateText] = [[plateText, plateImage, plateBox, fullImage, videoPath, frameNumber]]
+
+  def removeOldPlatesFromHistory(self, frameNumber):
+    # remove entries from rollingPlateDict that are older
+    # than rollingDictHistoryLimit frames
+    rollingPlateDictNew = {}
+    for plateText in self.rollingPlateDict.keys():
+      for plateEntry in self.rollingPlateDict[plateText]:
+        if (frameNumber - plateEntry[5]) < self.rollingDictHistoryLimit:
+          if plateText in rollingPlateDictNew.keys():
+            rollingPlateDictNew[plateText].append(plateEntry)
+          else:
+            rollingPlateDictNew[plateText] = [plateEntry]
+    self.rollingPlateDict = rollingPlateDictNew
+
+  # Add new plates to the rollingPlateDict
+  # Group plates that have similar plate text
+  # Select the "best" plate from the group, and delete the others
+  # Return the dictionary of best plates
+  def selectTheBestPlates(self):
+
+    # combine dictionary keys that match by at least 5 chars
+    combinedSimilarPlateCnt = 0
+    plateDictDeDuped = copy.deepcopy(self.rollingPlateDict)
+    plateDict2 = copy.deepcopy(self.rollingPlateDict)
+    for plateText1 in self.rollingPlateDict.keys():
+      del plateDict2[plateText1]
+      for plateText2 in plateDict2.keys():
+        matchCnt = 0
+        for i in np.arange(len(plateText1)):
+          if plateText1[i] == plateText2[i]:
+            matchCnt += 1
+        if matchCnt >= 5:
+          # check if plateText2 is in the plateDictDuped. If not then it has already been claimed as a duplicate by
+          # another plate, and is not available.
+          # If it is available, then copy to dict values at plateText1 and remove plateText2 key
+          if plateText2 in plateDictDeDuped.keys():
+            entryAdds = self.rollingPlateDict[plateText2]
+            for entryAdd in entryAdds:
+              # if plateText1 key has already been deleted, then add it back again
+              if plateText1 in plateDictDeDuped.keys():
+                plateDictDeDuped[plateText1].append(entryAdd)
+              else:
+                plateDictDeDuped[plateText1] = [entryAdd]
+                combinedSimilarPlateCnt -= 1
+            del plateDictDeDuped[plateText2]
+            combinedSimilarPlateCnt += 1
+
+    # The previous code grouped plates that matched by at least 5 chars under the same dictionary key
+    # However the dictionary key may not be the best plateText prediction for the group
+    # Now we change the key so it represents the most popular combination of characters for each group
+    plateDictPred = {}
+    for plateText in plateDictDeDuped.keys():
+      if len(plateDictDeDuped[plateText]) == 1:
+        # if there is only one plate, then copy to predicted plates
+        plateDictPred[plateText] = plateDictDeDuped[plateText]
+      else:
+        # For each char position build a histogram of character frequencies
+        charDicts = [{}, {}, {}, {}, {}, {}, {}]
+        for plateEntry in plateDictDeDuped[plateText]:
+          for (i,char) in enumerate(plateEntry[0]):
+            if char in charDicts[i].keys():
+              charDicts[i][char] += 1
+            else:
+              charDicts[i][char] = 1
+
+        # for each histogram, sort and then select the most frequently occurring characters
+        predPlateText =[]
+        for (i,charDict) in enumerate(charDicts):
+          plateTuples = charDict.items()
+          plateChars = sorted(plateTuples, key=lambda x:x[1] )
+          predPlateText.append(plateChars[-1][0])
+        predPlateText = ''.join(predPlateText)
+
+        # Copy dictionary entries from plateDictDeDuped, but use the new predicted plate text as the key
+        plateDictPred[predPlateText] = plateDictDeDuped[plateText]
+
+    # we only need one entry per plateText key, but let's try to pick the "best" entry
+    # ie find the entry where the plateText is closest to the key
+    bestMatchCnt = 0
+    plateDictBest = {}
+    for plateText in plateDictPred:
+      for plateEntry in plateDictPred[plateText]:
+        matchCnt = 0
+        plateTextEntry = plateEntry[0]
+        for i in np.arange(len(plateTextEntry)):
+          if plateText[i] == plateTextEntry[i]:
+            matchCnt += 1
+        # if this is entry has the best match to the key, then save it
+        # We use >= so that we can catch the case where matchCnt = 0
+        if matchCnt >= bestMatchCnt:
+          bestMatchCnt = matchCnt
+          bestPlateEntry = plateEntry
+          plateDictBest[plateText] = bestPlateEntry
+
+
+    return (plateDictBest)
+
+  def logToFile(self, plateDict, destFolderRootName):
+
+    for plateText in plateDict.keys():
+      (plateText, plateImage, plateBox, fullImage, videoPath, frameNumber) = plateDict[plateText]
+
+      # strip the video file name for use when saving still images
+      videoFileName = videoPath.split("/")[-1]
+      fileNamePrefix = videoPath.split("/")[-1].split("[")[0]
+
+      # create unique file names for the full image file and the cropped image file. Append plate text to the file name
+      outputCroppedFileName = "{}_{}".format(fileNamePrefix, self.fileCnt)
+      outputCroppedPath = "{}/{}/{}.png".format(self.output_cropped_image_path, destFolderRootName, outputCroppedFileName + '_' + plateText + '_1')
+      outputFullImageFileName = "{}_{}_{}".format(fileNamePrefix, self.fileCnt, plateText)
+      outputFullImagePath = "{}/{}/{}.jpg".format(self.output_image_path, destFolderRootName, outputFullImageFileName)
+      self.fileCnt += 1
+
+      # write cropped plate image to file
+      # Mimic the file name format used by Supervisely (almost)
+      cv2.imwrite(outputCroppedPath, plateImage)
+
+      # optionally annotate the image
+      if self.saveAnnotatedImage == True:
+        cv2.rectangle(fullImage, plateBox[0], plateBox[1], (0, 255, 0), 2)
+        cv2.putText(fullImage, plateText, plateBox[0], cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 2)
+      # print the plate text
+      print("[INFO] Found plate: {}".format(plateText))
+
+      # save the image
+      print("[INFO] logging image to file: {}".format(outputFullImagePath))
+      cv2.imwrite(outputFullImagePath, fullImage)
+
+      # update the log file
+      # videoFileName, imageFileName, date, time, frameNumber, plateText
+      # lplate_toy_video4.mp4,2018_01_01/lplate_toy_video.mp4_51.jpg,2018_01_10,5:05,271,5HUY634,5JHY768
+      imageFileName = "{}/{}.jpg".format(destFolderRootName, outputFullImageFileName)
+      date = destFolderRootName
+      m = re.search(r"^([0-9]{2}[.:][0-9]{2}[.:][0-9]{2})",videoFileName)
+      if m:
+        time = m.group(1)
+      else:
+        time = "HH.MM.SS"
+      self.logFile.write("{},{},{},{},{},{}\n".format(videoFileName, imageFileName, date, time, frameNumber, plateText))
+      self.logFile.flush()
